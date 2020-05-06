fdebf4b405fc219d01312ed4c1502ecb743eb661
Luke Huggett <smu12f_man@hotmail.com>
2020-05-06 11:11:43 +0800

    Added project to repo

---
+++ Models/ValueItem.cs
@@ -0,0 +1,8 @@
+﻿namespace DBReach.Models
+{
+    public class ValueItem
+    {
+        public long Id { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ Models/ValueItemContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace DBReach.Models
+{
+    public class ValueItemContext : DbContext
+    {
+        public ValueItemContext(DbContextOptions<ValueItemContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ValueItem> ValueItems { get; set; }
+    }
+}
